e6a146806299ef7413d103aac684efff46060849
Henrik Juhl (hjemme) <henrik.juhl_dk@hotmail.com>
2022-05-12 18:44:00 +0200

    small sizing changes to match the mobile screenshot included with product

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,18 @@
 const toggleMenuButton = document.querySelector("#toggle-menu-button");
 toggleMenuButton.addEventListener("click", (e) => {
   const btn = e.target;
+  navLinks = document.querySelector("#nav-links");
   if (btn.classList.contains("collapsed")) {
     // EXPAND NOW
     btn.classList.add("expanded");
     btn.classList.remove("collapsed");
+    navLinks.classList.add("expanded");
+    navLinks.classList.remove("collapsed");
   } else if (btn.classList.contains("expanded")) {
     // COLLAPSE NOW
     btn.classList.add("collapsed");
     btn.classList.remove("expanded");
+    navLinks.classList.add("collapsed");
+    navLinks.classList.remove("expanded");
   }
 });
